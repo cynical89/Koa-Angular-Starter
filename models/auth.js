ac1d1218241af918c81f40b8a7233d157fbb2dc6
Anthony Michelizzi <cynical89@gmail.com>
2017-03-13 14:10:26 -0700

    base scripts

---
+++ models/auth.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const passport = require("../index.js").passport;
+const config = require("../config.json");
+// const userModel = require("./user");
+const co = require("co");
+const bcrypt = require("bcryptjs");
+
+passport.serializeUser((user, done) => {
+	done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+	done(null, user);
+});
+
+const LocalStrategy = require("passport-local").Strategy;
+// if we have a port other than 80, add it to our callback url
+let port = "";
+if (config.site.port !== 80) {
+	port = `:${config.site.port}`;
+}
+passport.use(new LocalStrategy(
+	(username, password, done) => {
+		co(function* auth() {
+			// get the user
+			const user = yield userModel.get(username);
+			// const salt = yield bcrypt.genSalt(10);
+			// const hash = yield bcrypt.hash(password, salt);
+			if (user.error === true) {
+				// this user doesn't exist
+				return done(null, false);
+			}
+			const match = yield bcrypt.compare(password, user.password);
+			if (match !== true) {
+				return done(null, false);
+			}
+			done(null, user);
+		}).catch(function onError(e) {
+			console.error("Something went terribly wrong!");
+			console.error(e.stack);
+			done(e, null);
+		});
+	}
+));
---
+++ routes.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const config = require("./config.json");
+
+const app = require("./index").app;
+const passport = require("./index").passport;
+const Router = require("koa-router");
+const fs = require("fs");
+
+const routes = new Router();
+
+const main = require("./controllers/main");
+
+function loadHtml() {
+	return new Promise(function(resolve, reject) {
+		fs.readFile("./dist/index.html", {
+			"encoding": "utf8"
+		}, function(err, data) {
+			if (err) return reject(err);
+			resolve(data);
+		});
+	});
+};
+
+if (process.env.NODE_ENV === "production") {
+	routes.get(/^\/(.*)(?:\/|$)/, function* (next) {
+		if (this.request.url.startsWith("/api")) {
+			yield next;
+		} else {
+			this.body = yield loadHtml();
+		}
+	});
+}
+
+// routes
+
+// login routes
+routes.get("/api/login", main.login);
+routes.post("/api/login", function* (next) {
+	const ctx = this;
+	yield passport.authenticate("local", function* (err, user, info) {
+		if (err) throw err;
+		if (user === false) {
+			ctx.status = 401;
+			ctx.body = {
+				success: false
+			};
+		} else {
+			yield ctx.login(user);
+			ctx.body = {
+				success: true
+			};
+		}
+	}).call(this, next);
+});
+
+app.use(routes.middleware());
